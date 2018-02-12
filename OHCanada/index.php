0f546c0fd565fcb6e1c09ea2b6ff8ec262a562bd
vivies_a <antoine.vivies@epitech.eu>
2018-02-12 18:28:22 +0100

    https jQuery

--- OHCanada/index.php
+++ OHCanada/index.php
@@ -4,10 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<script
-	src="http://code.jquery.com/jquery-3.3.1.js"
-	integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-	crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<title>Rimouski</title>
 </head>
 <body>
