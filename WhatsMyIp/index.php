7a1d308dcc93388c6c52e9b4590ea6866c6034d6
Guillaume Rousseau <guillaume.rousseau@epitech.eu>
2018-02-12 18:34:30 +0100

    new chall

--- WhatsMyIp/index.php
+++ WhatsMyIp/index.php
@@ -1,14 +1,20 @@
+<?php
+if ($_GET["ip"] == "176.31.182.36")
+{
+    echo "The Flag is: omgitsthechangip";
+}
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Hidden form</title>
+        <title>Whats My Ip</title>
     </head>
     <body>
-        <form>
-            <input type="hidden" value="hiddenflag" name="Flag"></input>
-            <label>Nothing to submit</label>
+        <form method="GET" action=".">
+            <label>Ip :</label>
+            <input type="text" value="" name="ip"></input>
             <button>Submit</button>
         </form>
     </body>
